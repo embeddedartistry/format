9fc6a4bf38e0b788ab17b9c7646e4c8fb30c600e
Phillip Johnston <phillip@embeddedartistry.com>
2017-09-07 10:40:30 -0700

    Delete if size is 0

--- clang-format-patch.sh
+++ clang-format-patch.sh
@@ -4,4 +4,10 @@ clang-format -style=file -fallback-style=none -i `git ls-files -om "*.[ch]" "*.[
 
 git diff > clang_format.patch
 
+# Delete if 0 size
+if [ ! -s clang_format.patch ]
+then
+	rm clang_format.patch
+fi
+
 exit 0

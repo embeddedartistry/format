80a712c8a21aba744b950298e9cdeae1fbe9274e
Phillip Johnston <phillip@embeddedartistry.com>
2017-09-07 10:36:59 -0700

    Try a different approach based on testing in the EA repository

--- clang-format-patch.sh
+++ clang-format-patch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-clang-format -style=file -fallback-style=none `git ls-files -om "*.[ch]" "*.[hc]pp"` > format_patch.diff
+clang-format -style=file -fallback-style=none -i `git ls-files -om "*.[ch]" "*.[hc]pp"`
+
+git diff > clang_format.patch
 
 exit 0
